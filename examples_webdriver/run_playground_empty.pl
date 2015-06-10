c2ac9a9c07a6d1cd572121d798486671ddfa3069
Tim Vroom <vroom@blockstackers.com>
2015-06-10 16:26:20 -0600

    Make run_playground_empty really empty (of solving tasks)

--- examples_webdriver/run_playground_empty.pl
+++ examples_webdriver/run_playground_empty.pl
@@ -4,17 +4,25 @@ my $driver = Selenium::Remote::Driver->new();
 
 $driver->get("http://timvroom.com/selenium/playground");
 
-#15
-$driver->set_implicit_wait_timeout(11000);
-$driver->find_element('click then wait', 'link_text')->click();
-$driver->find_element('click after wait', 'link_text')->click();
-print "Completed #15\n";
 
-$driver->accept_alert();
-print "Completed #16\n";
+
+my $redbox_class = $driver->find_element_by_id('redbox')->get_attribute('class');
+$driver->find_element_by_id('answer6')->send_keys($redbox_class);
+
+#8
+my $js_return = $driver->execute_script("return got_return_from_js_function()");
+$driver->find_element_by_id('answer8')->send_keys($js_return);
+
+
+#11 
+my $orange_location = $driver->find_element_by_id('orangebox')->get_element_location();
+my $green_location = $driver->find_element_by_id('greenbox')->get_element_location();
+
+my $arrange_answer = $green_location->{y} < $orange_location->{y} ? "green" : "orange";
+$driver->find_element_by_id('answer11')->send_keys($arrange_answer);
+
 
 $driver->find_element_by_id('checkresults')->click();
-
 
 
 my $tophead = $driver->find_element_by_id('tophead');
